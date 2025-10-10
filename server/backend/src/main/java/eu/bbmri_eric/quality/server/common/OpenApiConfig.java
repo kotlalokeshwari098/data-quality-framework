5ba374f1da998b9c8641b5569e9178cfcfd37150
Radovan Tomasik <tomasik.radovan@gmail.com>
2025-10-10 23:42:47 +0200

    feat(server): add bearer auth security context to swagger

--- server/backend/src/main/java/eu/bbmri_eric/quality/server/common/OpenApiConfig.java
+++ server/backend/src/main/java/eu/bbmri_eric/quality/server/common/OpenApiConfig.java
@@ -1,11 +1,11 @@
 package eu.bbmri_eric.quality.server.common;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,7 +33,8 @@ class OpenApiConfig {
                         .url("https://www.gnu.org/licenses/agpl-3.0.en.html")))
         .components(
             new Components()
-                .addSecuritySchemes(securitySchemeName,
+                .addSecuritySchemes(
+                    securitySchemeName,
                     new SecurityScheme()
                         .name(securitySchemeName)
                         .type(SecurityScheme.Type.HTTP)
