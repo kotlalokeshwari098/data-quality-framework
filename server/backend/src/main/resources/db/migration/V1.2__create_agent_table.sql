b3582260a749b26de034189fb60b1e170495e5e7
Radovan Tomasik <tomasik.radovan@gmail.com>
2025-10-10 23:46:25 +0200

    fix(server): fix db migration

--- server/backend/src/main/resources/db/migration/V1.2__create_agent_table.sql
+++ server/backend/src/main/resources/db/migration/V1.2__create_agent_table.sql
@@ -12,3 +12,6 @@ CREATE INDEX idx_agent_status ON agent(status);
 ALTER TABLE user_account ADD COLUMN agent_id VARCHAR(36);
 ALTER TABLE user_account ADD CONSTRAINT fk_user_account_agent
     FOREIGN KEY (agent_id) REFERENCES agent(id);
+delete from user_account;
+INSERT INTO user_account (username, password) VALUES
+    ('admin', '$argon2id$v=19$m=19456,t=2,p=1$SQGK8wXpQw5b+qjuq/Ih1A$WP87YsUIErq6O+7rMk65U0cH4OHBRdrnM3yIG50gwpE');
