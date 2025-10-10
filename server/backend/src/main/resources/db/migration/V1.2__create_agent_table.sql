6fbff73dffc6728fe9769c006eb62113446750c1
Radovan Tomasik <tomasik.radovan@gmail.com>
2025-10-10 23:54:03 +0200

    fix(server): fix db migration

--- server/backend/src/main/resources/db/migration/V1.2__create_agent_table.sql
+++ server/backend/src/main/resources/db/migration/V1.2__create_agent_table.sql
@@ -15,3 +15,9 @@ ALTER TABLE user_account ADD CONSTRAINT fk_user_account_agent
 delete from user_account;
 INSERT INTO user_account (username, password) VALUES
     ('admin', '$argon2id$v=19$m=19456,t=2,p=1$SQGK8wXpQw5b+qjuq/Ih1A$WP87YsUIErq6O+7rMk65U0cH4OHBRdrnM3yIG50gwpE');
+-- Assign both ADMIN and HUMAN_USER roles to the admin user
+INSERT INTO user_roles (user_id, role)
+SELECT id, role
+FROM user_account
+         CROSS JOIN (VALUES ('ADMIN'), ('HUMAN_USER')) AS roles(role)
+WHERE username = 'admin';
