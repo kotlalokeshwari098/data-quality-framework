6724ad5fe6ceba15305a2b31f0971111c948df65
Radovan Tomasik <tomasik.radovan@gmail.com>
2025-11-03 16:00:14 +0100

    feat(agent): disable default repo exposure

--- agent/backend/src/main/java/eu/bbmri_eric/quality/agent/common/OpenApiConfig.java
+++ agent/backend/src/main/java/eu/bbmri_eric/quality/agent/common/OpenApiConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +15,8 @@ class OpenApiConfig {
 
   @Bean
   OpenAPI customOpenAPI() {
+    final String securitySchemeName = "bearerAuth";
+
     return new OpenAPI()
         .info(
             new Info()
@@ -35,8 +36,11 @@ class OpenApiConfig {
         .components(
             new Components()
                 .addSecuritySchemes(
-                    "basicAuth",
-                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
-        .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
+                    securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
   }
 }
