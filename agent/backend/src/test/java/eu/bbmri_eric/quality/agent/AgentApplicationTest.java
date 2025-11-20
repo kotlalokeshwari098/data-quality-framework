bcc41dee9c9747b6dfd5e262de5fb0f93070c097
Radovan Tomasik <tomasik.radovan@gmail.com>
2025-11-20 12:59:41 +0100

    refactor(agent): add UML diagram generation for modules

--- agent/backend/src/test/java/eu/bbmri_eric/quality/agent/AgentApplicationTest.java
+++ agent/backend/src/test/java/eu/bbmri_eric/quality/agent/AgentApplicationTest.java
@@ -3,12 +3,24 @@ package eu.bbmri_eric.quality.agent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class AgentApplicationTest {
 
+  ApplicationModules modules = ApplicationModules.of(AgentApplication.class);
+
   @Test
   void contextLoads() {
     ApplicationModules.of(AgentApplication.class).verify();
+  }
+
+  @Test
+  void writeDocumentationSnippets() {
+    try {
+      new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
+    } catch (Exception e) {
+      System.out.println("Could not write individual modules as plantuml");
+    }
   }
 }
